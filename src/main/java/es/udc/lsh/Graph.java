69465bccf254797e1292f3a411faa6c122ee1555
David Martinez Rego <davidmartinezrego@dmartinezrego-mac.cs.ucl.ac.uk>
2015-10-30 17:20:25 +0000

    Added first code bits to set the structure

---
+++ src/main/java/es/udc/lsh/Graph.java
@@ -0,0 +1,7 @@
+package es.udc.lsh;
+
+/**
+ * Created by davidmartinezrego on 30/10/15.
+ */
+public class Graph {
+}
---
+++ src/main/scala/es/udc/graph/Graph.scala
@@ -0,0 +1,8 @@
+package es.udc.graph
+
+/**
+ * Created by davidmartinezrego on 30/10/15.
+ */
+class Graph {
+
+}
