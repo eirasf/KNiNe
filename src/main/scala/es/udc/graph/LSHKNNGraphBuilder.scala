6b56d5887099798506184f0093bf307a35e6e25a
eirasf <carlos.eiras.franco@udc.es>
2019-01-09 12:33:20 +0100

    Sped up last step.

--- src/main/scala/es/udc/graph/LSHKNNGraphBuilder.scala
+++ src/main/scala/es/udc/graph/LSHKNNGraphBuilder.scala
@@ -64,7 +64,8 @@ var numBuckets:Long=2
     
     //while(!currentData.isEmpty())
     //while(nodesLeft>numNeighbors)
-    while((numBuckets>1 || nodesLeft>numNeighbors) && nodesLeft>1)
+    //while((numBuckets>1 || nodesLeft>numNeighbors) && nodesLeft>1)
+    while(nodesLeft>numNeighbors && nodesLeft>1)
     {
       //Maps each element to numTables (hash, index) pairs with hashes of keyLenght length.
       val hashRDD=currentData.flatMap({case (index, point) =>
